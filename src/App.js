49d5a32ae24e351074454aa3720c566028030bd8
N10T <andy.tisba@gmail.com>
2021-03-20 19:34:01 +0100

    finish junior part

--- src/App.js
+++ src/App.js
@@ -1,96 +1,106 @@
 import SVG from "./components/SVG";
+import Can from "./components/Can";
+import Tea from "./components/Tea";
+import Bubble from "./components/Bubble";
 import "./App.scss";
-import { useMemo, useState } from "react";
+import { useCallback, useState, useRef, useEffect } from "react";
+
+const palette = {
+  tea: ["#FE8261", "#9A5B46", "#9DBEA2"],
+  bubble: ["#8F3939", "#FFCE9F", "#3A3B59"],
+  can: ["#FF728A", "#FFEF72", "#7280FF"],
+};
 
 function App() {
-  const [bubble, setBubble] = useState("#8F3939");
-  const [background, setBackground] = useState("#FE8261");
-  const [tube, setTube] = useState(false);
-  const [tea, setTea] = useState("#FE8261");
-  const [border, setBorder] = useState("#FE8261");
-  const [smile, setSmile] = useState(false);
+  const colors = {
+    tea: useState({ color: palette.tea[0], index: 0 }),
+    bubble: useState({ color: palette.bubble[0], index: 0 }),
+    can: useState({ color: palette.can[0], index: 0 }),
+  };
+  const [tea] = colors.tea;
+  const [bubble] = colors.bubble;
+  const [can] = colors.can;
   const [size, setSize] = useState(50);
-  const [name, setName] = useState("Tea");
+  const [tube, setTube] = useState(false);
+  const [title, setName] = useState("Thé matcha");
+  const [smile, setSmile] = useState(false);
 
-  const color = useCallback(
-    () => ({
-      tea: ["#FE8261", "#9A5B46", "#9DBEA2"],
-      bubble: ["#8F3939", "#FFCE9F", "#3A3B59"],
-      smile: ["#FF728A", "#FFEF72", "#7280FF"],
-    }),
-    []
-  );
+  const titleRef = useRef(null)
+  const handleClick = useCallback((e) => {
+    e.preventDefault();
+    colors[e.target.name][1]((oldValue) => {
+      const name = e.target.name;
+      const index = (oldValue.index + 1) % 3;
+      const newColor = palette[name][index];
+      return { color: newColor, index };
+    });
+  }, []);
+
 
   return (
     <div id="App">
       <div id="displayer" className="box">
         <SVG
-          bubble={bubble}
-          size={size}
-          background={background}
-          tea={tea}
-          isTube={tube}
-          border={border}
+          bubble={bubble.color}
+          can={can.color}
           isSmile={smile}
+          isTube={tube}
+          size={size}
+          tea={tea.color}
         />
-        <h1 className="name">{name}</h1>
+        <h1 className="name" ref={titleRef}>{title}</h1>
       </div>
       <div id="sidebar" className="box">
         <h1 className="sidebar__title">Bubble tea generator</h1>
         <div className="sidebar_inputs-container">
           <div className="sidebar__input-wrapper">
-            <label htmlFor="" className="label">
-              nom
-            </label>
+            <label className="label">nom</label>
             <input
               type="text"
               className="input"
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => {
+                titleRef.current.style.fontFamily =`var(--font-family-${Math.floor(Math.random()*5)})`
+                setName(e.target.value)}}
               name="Name"
-              value={name}
+              value={title}
               placeholder="Tea"
             />
           </div>
           <div className="sidebar__input-wrapper">
-            <label htmlFor="" className="label">
-              gobelet
-            </label>
+            <label className="label">gobelet</label>
             <input
               type="color"
               className="input"
-              onChange={(e) => setBorder(e.target.value)}
-              name="border"
-              value={border}
+              onClick={handleClick}
+              name="can"
+              value={can.color}
+              readOnly
             />
           </div>
           <div className="sidebar__input-wrapper">
-            <label htmlFor="" className="label">
-              tea
-            </label>
+            <label className="label">tea</label>
             <input
               type="color"
               className="input"
-              onChange={(e) => setTea(e.target.value)}
+              onClick={handleClick}
               name="tea"
-              value={tea}
+              value={tea.color}
+              readOnly
             />
           </div>
           <div className="sidebar__input-wrapper">
-            <label htmlFor="" className="label">
-              tapioca
-            </label>
+            <label className="label">tapioca</label>
             <input
               type="color"
               className="input"
-              onChange={(e) => setBubble(e.target.value)}
+              onClick={handleClick}
               name="bubble"
-              value={bubble}
+              value={bubble.color}
+              readOnly
             />
           </div>
           <div className="sidebar__input-wrapper">
-            <label htmlFor="" className="label">
-              taille
-            </label>
+            <label className="label">taille</label>
             <input
               type="range"
               className="input"
@@ -102,35 +112,37 @@ function App() {
             />
           </div>
           <div className="sidebar__input-wrapper">
-            <label htmlFor="" className="label">
-              paille
-            </label>
+            <label className="label">paille</label>
             <input
               type="checkbox"
               className="input"
               onChange={(e) => setTube(e.target.checked)}
-              name="smile"
+              name="tube"
               checked={tube}
             />
           </div>
           <div className="sidebar__input-wrapper">
-            <label htmlFor="" className="label">
-              sourire
-            </label>
+            <label className="label">sourire</label>
             <input
               type="checkbox"
               className="input"
               onChange={(e) => setSmile(e.target.checked)}
-              name="name"
+              name="smile"
               checked={smile}
             />
           </div>
         </div>
       </div>
       <div id="bottombar" className="box">
-        <div className="Box"></div>
-        <div className="Box"></div>
-        <div className="Box"></div>
+        <div className="bottombar__Box">
+          <Tea tea={tea.color} />
+        </div>
+        <div className="bottombar__Box">
+          <Can can={can.color} />
+        </div>
+        <div className="bottombar__Box">
+          <Bubble bubble={bubble.color} />
+        </div>
       </div>
     </div>
   );
