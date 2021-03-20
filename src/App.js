9cd25b70a4800bee4330b197f5265f79b7b1384c
N10T <andy.tisba@gmail.com>
2021-03-20 16:56:24 +0100

    no ref | no input color

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import logo from "./logo.svg";
 import SVG from "./components/SVG";
 import "./App.scss";
-import { useRef, useState } from "react";
+import { useMemo, useState } from "react";
 
 function App() {
-  const ref = {
-    bubble: useRef(null),
-    background: useRef(null),
-    tea: useRef(null),
-    border: useRef(null),
-    smile: useRef(null),
-  };
-  const [bubble, setBubble] = useState(initialState)
-  const [background, setBackground] = useState(initialState)
-  const [tea, setTea] = useState(initialState)
-  const [border, setBorder] = useState(initialState)
-  const [smile, setSmile] = useState(initialState)
-  const handleInput = (e) => {
-    if (e.target.type !== "checkbox") {
-      console.log(e.target.value);
-    } else {
-      console.log(e.target.value);
-    }
-  };
-  // const [state, setstate] = useState(initialState);
+  const [bubble, setBubble] = useState("#8F3939");
+  const [background, setBackground] = useState("#FE8261");
+  const [tube, setTube] = useState(false);
+  const [tea, setTea] = useState("#FE8261");
+  const [border, setBorder] = useState("#FE8261");
+  const [smile, setSmile] = useState(false);
+  const [size, setSize] = useState(50);
+  const [name, setName] = useState("Tea");
+
+  const color = useCallback(
+    () => ({
+      tea: ["#FE8261", "#9A5B46", "#9DBEA2"],
+      bubble: ["#8F3939", "#FFCE9F", "#3A3B59"],
+      smile: ["#FF728A", "#FFEF72", "#7280FF"],
+    }),
+    []
+  );
+
   return (
     <div id="App">
       <div id="displayer" className="box">
-        <SVG allRef={ref} />
+        <SVG
+          bubble={bubble}
+          size={size}
+          background={background}
+          tea={tea}
+          isTube={tube}
+          border={border}
+          isSmile={smile}
+        />
+        <h1 className="name">{name}</h1>
       </div>
       <div id="sidebar" className="box">
         <h1 className="sidebar__title">Bubble tea generator</h1>
@@ -36,43 +42,88 @@ function App() {
             <label htmlFor="" className="label">
               nom
             </label>
-            <input type="text" className="input" onChange={handleInput}  name="name"/>
+            <input
+              type="text"
+              className="input"
+              onChange={(e) => setName(e.target.value)}
+              name="Name"
+              value={name}
+              placeholder="Tea"
+            />
           </div>
           <div className="sidebar__input-wrapper">
             <label htmlFor="" className="label">
               gobelet
             </label>
-            <input type="color" className="input" onChange={handleInput}  name="border"/>
+            <input
+              type="color"
+              className="input"
+              onChange={(e) => setBorder(e.target.value)}
+              name="border"
+              value={border}
+            />
           </div>
           <div className="sidebar__input-wrapper">
             <label htmlFor="" className="label">
               tea
             </label>
-            <input type="color" className="input" onChange={handleInput}  name="tea"/>
+            <input
+              type="color"
+              className="input"
+              onChange={(e) => setTea(e.target.value)}
+              name="tea"
+              value={tea}
+            />
           </div>
           <div className="sidebar__input-wrapper">
             <label htmlFor="" className="label">
               tapioca
             </label>
-            <input type="color" className="input" onChange={handleInput}  name="bubble"/>
+            <input
+              type="color"
+              className="input"
+              onChange={(e) => setBubble(e.target.value)}
+              name="bubble"
+              value={bubble}
+            />
           </div>
           <div className="sidebar__input-wrapper">
             <label htmlFor="" className="label">
               taille
             </label>
-            <input type="range" className="input" onChange={handleInput} name="size" />
+            <input
+              type="range"
+              className="input"
+              max="100"
+              min="30"
+              onChange={(e) => setSize(e.target.value)}
+              name="size"
+              value={size}
+            />
           </div>
           <div className="sidebar__input-wrapper">
             <label htmlFor="" className="label">
               paille
             </label>
-            <input type="checkbox" className="input" onChange={handleInput}  name="smile"/>
+            <input
+              type="checkbox"
+              className="input"
+              onChange={(e) => setTube(e.target.checked)}
+              name="smile"
+              checked={tube}
+            />
           </div>
           <div className="sidebar__input-wrapper">
             <label htmlFor="" className="label">
               sourire
             </label>
-            <input type="checkbox" className="input" onChange={handleInput} name="name" />
+            <input
+              type="checkbox"
+              className="input"
+              onChange={(e) => setSmile(e.target.checked)}
+              name="name"
+              checked={smile}
+            />
           </div>
         </div>
       </div>
